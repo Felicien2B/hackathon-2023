c826009f8be33964492b0323e18da1436534f4bc
Akram amrani <144606449+Gahraman17@users.noreply.github.com>
2023-12-21 19:10:23 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,34 +1,30 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-# Une liste d'exemple d'articles
 articles = [
-    {'id': 3, 'domain': 'Python', 'abstract': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nam aliquam sem et tortor. Ac odio tempor orci dapibus. Nibh cras pulvinar mattis nunc sed blandit libero. Arcu dui vivamus arcu felis bibendum. Vulputate eu scelerisque felis imperdiet proin fermentum leo. Ac auctor augue mauris augue neque gravida in fermentum. Dictum sit amet justo donec enim diam vulputate.', 'keywords': ['python', 'flask']},
-    {'id': 8, 'domain': 'Web', 'abstract': 'Contenu de l\'article 8', 'keywords': ['web', 'development']},
-    {'id': 15, 'domain': 'Data Science', 'abstract': 'Contenu de l\'article 15', 'keywords': ['data', 'science']},
+    {'id': 3, 'title': 'Article 1', 'content': 'Le modèle vectoriel est relativement simple à appréhender (algèbre linéaire) et est facile à implémenter. Il permet de retrouver assez efficacement des documents dans un corpus non structuré (recherche dinformation), son efficacité dépendant pour une grande part de la qualité de la représentation (vocabulaire et schéma de pondération). La représentation vectorielle permet aussi une mise en correspondance des documents avec une requête imparfaite', 'keywords': ['python', 'flask']},
+    {'id': 8, 'title': 'Article 2', 'content': 'Contenu de l\'article 8', 'keywords': ['web', 'development']},
+    {'id': 15, 'title': 'Article 3', 'content': 'Contenu de l\'article 15', 'keywords': ['data', 'science']},
 ]
 
-# Routes de l'application
-
-# Recherche d'articles
 @app.route('/', methods=['GET', 'POST'])
 def search():
-    domains = list(set(article['domain'] for article in articles))
+    matching_articles = []
     if request.method == 'POST':
-        selected_domain = request.form['domain']
-        matching_articles = [article for article in articles if article['domain'] == selected_domain]
-        return render_template('index.html', articles=matching_articles, domains=domains, selected_domain=selected_domain)
-    return render_template('index.html', domains=domains)
+        search_query = request.form['search'].lower()
+        matching_articles = [
+            article for article in articles
+            if any(keyword in search_query for keyword in article['keywords'])
+        ]
+    return render_template('index.html', articles=matching_articles)
 
-# Sélection d'un article
 @app.route('/article/<int:article_id>')
 def article(article_id):
     article = next((a for a in articles if a['id'] == article_id), None)
     if article:
         return render_template('article.html', article=article)
     return 'Article non trouvé', 404
-
 
 if __name__ == '__main__':
     app.run(debug=True)
