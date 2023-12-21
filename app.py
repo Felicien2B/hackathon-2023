f8c605919d7a30bc579e425ebc1183a23aedbbb4
liviagr <113026321+liviagr@users.noreply.github.com>
2023-12-21 23:51:10 +0100

    routes faites

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ from flask import Flask, render_template, request
 import chromadb
 from chromadb.config import Settings
 
+from Nearest_N_Articles import NearestNArticles
+
 app = Flask(__name__)
 
 # articles = [
@@ -65,6 +67,76 @@ def get_domain():
     res = f.read()
     f.close()
     return res
+
+# a vérifier (une fois qu'on a récupérer l'id de l'article depuis le front)
+# plus faire proprement
+@app.route('/get_article_connexe', methods=["GET", "POST"])
+def get_article_connexe():
+    chroma_client = chromadb.Client(Settings(chroma_db_impl="duckdb+parquet", persist_directory="hackathon-2023/bdd/"))
+    # collection permet de stocker embeddings, documents et autre metadata
+    collection = chroma_client.get_or_create_collection(name="article")
+    nb_article_connexe = 5
+    # request.form["relatedArticles"]
+    id_article = 1
+    
+    
+    res = NearestNArticles(id_article, nb_article_connexe)
+    
+    res = [str(x) for x in res]
+
+    
+    result = collection.get(ids=res)
+    
+    result_list = []
+    
+    for i in range(len(result["ids"])):
+        id = result["ids"][i]
+        keyword = result["metadatas"][i]["keywords"]
+        abstract = result["metadatas"][i]["abstract"]
+        result_list.append({
+            "id": id,
+            "keywords" : keyword,
+            "abstract" : abstract
+        })
+        
+        
+    return result_list
+
+
+# a vérifier + faire proprement
+@app.route('/get_article', methods=["GET", "POST"])
+def get_article():
+    chroma_client = chromadb.Client(Settings(chroma_db_impl="duckdb+parquet", persist_directory="hackathon-2023/bdd/"))
+    # collection permet de stocker embeddings, documents et autre metadata
+    collection = chroma_client.get_or_create_collection(name="article") 
+    
+    
+    if request.method == 'POST':
+        id_query = request.form['search'].lower()
+        result = collection.get(ids=id_query)
+    elif request.method == "GET":
+        result = collection.get()
+
+    result_list = []
+    id = result["ids"]
+    keyword = result["metadatas"]["keywords"]
+    abstract = result["metadatas"]["abstract"]
+    result_list.append({
+        "id": id,
+        "keywords" : keyword,
+        "abstract" : abstract
+    })
+    
+    return result_list
+        
+
+    
+
+    
+    
+    
+    
+    
        
 
 
--- test.py
+++ test.py
@@ -9,20 +9,28 @@ collection = chroma_client.get_or_create_collection(name="article")
 
 # result = collection.get(ids=["0"])
 
-result = collection.get()
+list_id = [1596, 3422, 4805, 1841, 4200]
+list_id = [str(x) for x in list_id]
+
+result = collection.get(ids=list_id)
 
 list_domain = []
 
-for i in range(len(result["ids"])):
-    list_domain.append(result["metadatas"][i]["area"])
+print(result["ids"])
+
+
+
+
+# for i in range(len(result["ids"])):
+#     list_domain.append(result["metadatas"][i]["area"])
     
 
-list_domain = np.unique(list_domain)
-# print(list_domain)
-list_domain_json = json.dumps(list(list_domain))
-f = open("hackathon-2023/src/list_domaine.json", "w")
-f.write(list_domain_json)
-f.close()
+# list_domain = np.unique(list_domain)
+# # print(list_domain)
+# list_domain_json = json.dumps(list(list_domain))
+# f = open("hackathon-2023/src/list_domaine.json", "w")
+# f.write(list_domain_json)
+# f.close()
 
 # print(result["metadatas"][0])
 # print(result.keys()) #432
