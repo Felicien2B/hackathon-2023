06c080bb066bf4dddf1518856bf9c8bf6ac289ee
Félicien <felicien.bertrand@gmail.com>
2023-12-21 18:18:54 +0100

    Ajout domains

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ app = Flask(__name__)
 
 # Une liste d'exemple d'articles
 articles = [
-    {'id': 1, 'title': 'Article 1', 'content': 'Contenu de l\'article 1', 'keywords': ['python', 'flask']},
-    {'id': 2, 'title': 'Article 2', 'content': 'Contenu de l\'article 2', 'keywords': ['web', 'development']},
-    {'id': 3, 'title': 'Article 3', 'content': 'Contenu de l\'article 3', 'keywords': ['data', 'science']},
+    {'id': 3, 'domain': 'Python', 'abstract': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nam aliquam sem et tortor. Ac odio tempor orci dapibus. Nibh cras pulvinar mattis nunc sed blandit libero. Arcu dui vivamus arcu felis bibendum. Vulputate eu scelerisque felis imperdiet proin fermentum leo. Ac auctor augue mauris augue neque gravida in fermentum. Dictum sit amet justo donec enim diam vulputate.', 'keywords': ['python', 'flask']},
+    {'id': 8, 'domain': 'Web', 'abstract': 'Contenu de l\'article 8', 'keywords': ['web', 'development']},
+    {'id': 15, 'domain': 'Data Science', 'abstract': 'Contenu de l\'article 15', 'keywords': ['data', 'science']},
 ]
 
 # Routes de l'application
@@ -14,12 +14,12 @@ articles = [
 # Recherche d'articles
 @app.route('/', methods=['GET', 'POST'])
 def search():
-    matching_articles = []
+    domains = list(set(article['domain'] for article in articles))
     if request.method == 'POST':
-        search_query = request.form['search']
-        # Filtrer les articles correspondant à la recherche
-        matching_articles = [article for article in articles if search_query.lower() in article['title'].lower()]
-    return render_template('index.html', articles=matching_articles)
+        selected_domain = request.form['domain']
+        matching_articles = [article for article in articles if article['domain'] == selected_domain]
+        return render_template('index.html', articles=matching_articles, domains=domains, selected_domain=selected_domain)
+    return render_template('index.html', domains=domains)
 
 # Sélection d'un article
 @app.route('/article/<int:article_id>')
