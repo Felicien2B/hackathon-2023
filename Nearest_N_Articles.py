3edc5e07d0b6054df4d443f9133170cad5603d08
liviagr <113026321+liviagr@users.noreply.github.com>
2023-12-22 13:27:18 +0100

    commentaires + nettoyage du code

--- Nearest_N_Articles.py
+++ Nearest_N_Articles.py
@@ -3,22 +3,34 @@ from scipy.spatial.distance import cosine
 import pandas as pd
 
 def NearestNArticles(idArticle, n):
+    """calcule les distances (avec cosine) entre les embeddings de idArticle et ceux des articles de la base
+    renvoie les n articles les plus proches
+
+    Args:
+        idArticle (int): représente l'id de l'article courant
+        n (int): représente le nombre d'articles connexes à renvoyer
+
+    Returns:
+        list[int]: list d'entiers représentants les id des n articles connexes
+    """
     df = pd.read_excel("hackathon-2023\Hackathon2023_CleanDataEmbedding.xlsx")
     z=idArticle
+    
+    # traitement pour récupérer les embeddings sous la forme de list de float
     embeddings = df['keywords_embeddings']
     df["new_embeddings"] = df["keywords_embeddings"].apply(lambda x: x.replace("[", "").replace("]", "").replace("\n", "").split())
     df["new_embeddings"] = df["new_embeddings"].apply(lambda x: [float(i) for i in x])
     query_embedding = [float(i) for i in embeddings[z].replace("[", "").replace("]", "").split()]
     
+    # calcule des distance entre l'article courant et les autres articles
     df["distances"] = df["new_embeddings"].apply(lambda x: cosine(query_embedding, x))
+    
+    # on récupère les plus petites distances
     smallest_distances = df.nsmallest(n+1, 'distances')[[ "keywords_embeddings", "distances"]]
     indices_of_smallest_distances = smallest_distances.index.tolist()
     
+    # conversion des index en string (pour la requête de la base)
     indices_of_smallest_distances = [str(x) for x in indices_of_smallest_distances]
 
-  
-    # return smallest_distances, indices_of_smallest_distances[1:]
     return indices_of_smallest_distances[1:]
     
-# res = NearestNArticles(1, 5)
-# print(res)
--- app.py
+++ app.py
@@ -13,18 +13,17 @@ collection = client.get_or_create_collection(name="article")
 print(collection.count())
 
 
-# articles = [
-#     {'id': 3, 'domain': 'Python', 'abstract': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', 'keywords': ['python', 'flask']},
-#     {'id': 8, 'domain': 'Web', 'abstract': 'Contenu de l\'article 2', 'keywords': ['web', 'development']},
-#     {'id': 15, 'domain': 'Data Science', 'abstract': 'Contenu de l\'article 3', 'keywords': ['data', 'science']},
-# ]
 
+# route par défaut
 @app.route('/', methods=['GET', 'POST'])
 def search():
     matching_articles = []
+    # renvoie les article en fonction du domaine spécifié en entrée
     if request.method == 'POST':
         search_query = request.form['search']
+        # récupère les articles du domaine
         result = collection.get(where={"area" : search_query})
+        # renvoie le résultat sous la forme : [{"id" : , "abstract" ; , "keywords" : }, {}]
         result_list = get_resultat_requete(result)
         print(result)
         print(result_list)
@@ -48,12 +47,9 @@ def article(article_id):
     return 'Article non trouvé', 404
 
 
-# renvoie la liste d'articles
+# renvoie la liste d'articles en fonciton du domaine spécifié
 @app.route('/list_article', methods=['GET', 'POST'])
 def get_list_article():
-    # chroma_client = chromadb.Client(Settings(chroma_db_impl="duckdb+parquet", persist_directory="hackathon-2023/bdd/"))
-    # # collection permet de stocker embeddings, documents et autre metadata
-    # collection = chroma_client.get_or_create_collection(name="article") 
     
     # pour tous les articles du domaine précisé
     if request.method == 'POST':
@@ -63,76 +59,45 @@ def get_list_article():
     elif request.method == "GET":
         result = collection.get()
 
-    # renvoie le résultat
+    # renvoie le résultat sous la forme : [{"id" : , "abstract" ; , "keywords" : }, {}]
     result_list = get_resultat_requete(result)
-    # for i in range(len(result["ids"])):
-    #     id = result["ids"][i]
-    #     keyword = result["metadatas"][i]["keywords"]
-    #     abstract = result["metadatas"][i]["abstract"]
-    #     result_list.append({
-    #         "id": id,
-    #         "keywords" : keyword,
-    #         "abstract" : abstract
-    #     })
         
     return result_list
 
-# a vérifier (une fois qu'on a récupérer l'id de l'article depuis le front)
-# plus faire proprement
+# renvoie les id des articles connexes à l'article courant
 @app.route('/get_article_connexe', methods=["POST"])
 def get_article_connexe():
-    # chroma_client = chromadb.Client(Settings(chroma_db_impl="duckdb+parquet", persist_directory="hackathon-2023/bdd/"))
-    # # collection permet de stocker embeddings, documents et autre metadata
-    # collection = chroma_client.get_or_create_collection(name="article")
+
+    # récupère le nombre d'article connexe
     nb_article_connexe = int(request.form["relatedArticles"])
     
-    
+    # récupère l'id de l'article courant
     id_article = int(request.form["idArticle"])
     
-    
+    # appelle la fonction pour récupérer un tableau des id des articles connexes
     res = NearestNArticles(id_article, nb_article_connexe)
     
-    # res = [str(x) for x in res]
+    # récupère les infos des articles connexes
     result = collection.get(ids=res)
     
+    # renvoie le résultat sous la forme : [{"id" : , "abstract" ; , "keywords" : }, {}]
     result_list = get_resultat_requete(result)
-    
-    # for i in range(len(result["ids"])):
-    #     id = result["ids"][i]
-    #     keyword = result["metadatas"][i]["keywords"]
-    #     abstract = result["metadatas"][i]["abstract"]
-    #     result_list.append({
-    #         "id": id,
-    #         "keywords" : keyword,
-    #         "abstract" : abstract
-    #     })
-        
         
     return result_list
 
 
-# a vérifier + faire proprement
+# renvoie un article grâce à son id
 @app.route('/get_article', methods=["POST"])
 def get_article():
-    # chroma_client = chromadb.Client(Settings(chroma_db_impl="duckdb+parquet", persist_directory="hackathon-2023/bdd/"))
-    # # collection permet de stocker embeddings, documents et autre metadata
-    # collection = chroma_client.get_or_create_collection(name="article") 
     
-    
-    if request.method == 'POST':
-        id_query = request.form['search'].lower()
-        result = collection.get(ids=[id_query])
+    # récupère l'id de l'article à afficher
+    id_query = request.form['search'].lower()
+    # récupère les infos de l'article
+    result = collection.get(ids=[id_query])
 
-
+    # renvoie le résultat sous la forme : [{"id" : , "abstract" ; , "keywords" : }, {}]
     result_list = get_resultat_requete(result)
-    # id = result["ids"]
-    # keyword = result["metadatas"][0]["keywords"]
-    # abstract = result["metadatas"][0]["abstract"]
-    # result_list.append({
-    #     "id": id,
-    #     "keywords" : keyword,
-    #     "abstract" : abstract
-    # })
+
     
     return result_list
         
